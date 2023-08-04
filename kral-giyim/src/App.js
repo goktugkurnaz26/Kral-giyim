ae801b55ccb88a6082e30957d62ab38f5bbc04e0
Gazi Göktuğ <115394107+goktugkurnaz26@users.noreply.github.com>
2023-08-04 13:25:42 +0300

    Add files via upload

---
+++ kral-giyim/src/App.js
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './routes/home/home.component'
+import Navigation from './routes/navigation/navigation.component'
+
+const Shop = () => {
+  return <h1>Ben mağaza sayfasıyım</h1>
+}
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
